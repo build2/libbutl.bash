1f2a98dec70699905cf1a4c9ba48ad6397448a9a
Boris Kolpackov <boris@codesynthesis.com>
2018-07-13 15:35:30 +0200

    Add manifest parser stub

--- libbutl/manifest-parser.bash
+++ libbutl/manifest-parser.bash
@@ -1,3 +1,13 @@
 # file      : libbutl/manifest-parser.bash
 # copyright : Copyright (c) 2014-2018 Code Synthesis Ltd
 # license   : MIT; see accompanying LICENSE file
+
+function parse_manifest ()
+{
+  printf ": 1\0"
+  printf "name: foo\0"
+  printf "version: 1.2.3\0"
+  printf "description: foo\nexecutable\0"
+  printf "depends: libfoo\0"
+  printf "depends: libbar\0"
+}
