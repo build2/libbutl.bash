357172f4409c74d7a2ac963290fc70c7ae10e3b1
Boris Kolpackov <boris@codesynthesis.com>
2018-07-20 14:35:28 +0200

    Switch to bash build2 module

--- libbutl/manifest-parser.bash
+++ libbutl/manifest-parser.bash
@@ -2,12 +2,12 @@
 # copyright : Copyright (c) 2014-2018 Code Synthesis Ltd
 # license   : MIT; see accompanying LICENSE file
 
-function parse_manifest ()
+function butl_parse_manifest ()
 {
-  printf ": 1\0"
-  printf "name: foo\0"
-  printf "version: 1.2.3\0"
-  printf "description: foo\nexecutable\0"
-  printf "depends: libfoo\0"
-  printf "depends: libbar\0"
+  printf ":1\0"
+  printf "name:foo\0"
+  printf "version:1.2.3\0"
+  printf "description:foo\nexecutable\0"
+  printf "depends:libfoo\0"
+  printf "depends:libbar\0"
 }
